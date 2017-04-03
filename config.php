4daf21f9e33767242a59bcca82b0e91bb3da2a56
maritng <martingrandi@gmail.com>
2017-04-02 21:56:15 -0300

    First version Moodle 30 as design

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'mood199';
+$CFG->dbuser    = 'mood199';
+$CFG->dbpass    = '](0S3PId4x';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8_general_ci',
+);
+
+$CFG->wwwroot   = 'http://127.0.0.1/moodle30';
+$CFG->dataroot  = 'C:/Program Files (x86)/Ampps/private/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
